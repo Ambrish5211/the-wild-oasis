2eccff2cfc5ede38cc3447c66c9220b03d6a1f30
Ambrish Kumar <ambrish19.5211@gmail.com>
2024-04-04 12:55:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>Hellow world</H1>
+        <Button onClick={() => alert("Check In")}>CheckIn</Button>
+        <Button onClick={() => alert("CheckOut")}>CheckOut</Button>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
